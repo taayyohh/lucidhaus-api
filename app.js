ef3228f041accd5cef1d71cbf5bdb9fdf2853120
Miles Bridges <mbridges@greatjakes.com>
2021-01-26 19:22:57 -0500

    prerender io

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(expressValidator())
 app.use(cors())
-app.use(require('prerender-node'));
+app.use(require('prerender-node').set('prerenderToken', 'cD4eXmwHZPwc2DVBZinP'));
 
 
 //routes
