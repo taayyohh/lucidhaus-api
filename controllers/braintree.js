d3c32599cfa41faac55b2a2e932efcfc76efd89c
Miles Bridges <mbridges@greatjakes.com>
2021-02-01 03:39:38 -0500

    prodction

--- controllers/braintree.js
+++ controllers/braintree.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 
 const gateway = braintree.connect({
-    environment: braintree.Environment.Sandbox,
+    environment: braintree.Environment.Production,
     merchantId: process.env.BRAINTREE_MERCHANT_ID,
     publicKey: process.env.BRAINTREE_PUBLIC_KEY,
     privateKey: process.env.BRAINTREE_PRIVATE_KEY
