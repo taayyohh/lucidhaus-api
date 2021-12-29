eca6df3ea7f7313fac7e5b45f7a7bd08a06b14cf
Théo Mode <milesxbridges@gmail.com>
2021-12-29 17:20:32 -0500

    switch to production braintree

--- features/shop/product/controllers/braintree.js
+++ features/shop/product/controllers/braintree.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const gateway = braintree.connect({
     // For Prod switch to Prod variables
-    environment: braintree.Environment.Sandbox,
+    environment: braintree.Environment.Production,
     merchantId: process.env.BRAINTREE_MERCHANT_ID,
     publicKey: process.env.BRAINTREE_PUBLIC_KEY,
     privateKey: process.env.BRAINTREE_PRIVATE_KEY,
